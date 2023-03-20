6cf36db9a8c959fb71b12e9181bb3854fbd81d2f
Vladnet <vinvlad@yandex.ru>
2023-03-20 17:51:48 +0300

    change final task

--- task_final/solution.js
+++ task_final/solution.js
@@ -36,27 +36,24 @@
 
 
 
-  
+   
 function sendRequest(name, phone, address, goods, sum)
     {
+  
+let data = {order: {address, sum}, client: {name, phone}, goods: []};
 
-    let data = {
-        client: name + " " + phone, 
-        goods: [], 
-        order: {address: "ул. " + address.street + ", дом " + address.house + ", " + address.entrance + " подъезд, " + address.floor + " этаж, кв." + address.flat, 
-        sum: sum},
-    };
+let countOfGoods = goods.length;
 
-let countOfGoods = goods.length;
-
-for (let i = 0; i < countOfGoods; i ++) {
-  data.goods.push({title: goods[i].title, count: goods[i].count});
+for (let i = 0; i < countOfGoods; i ++) {
+    data.goods.push({title: goods[i].title, count: goods[i].count});
 }
 
-data.order.address = "ул. " + address.street + ", дом " + address.house + " , " + address.entrance + " подъезд," + address.floor + " этаж, кв. " + address.flat;
-data.order.sum = sum;
-data.client = name + " " + phone;
-let jsonData = JSON.stringify({data});
-return jsonData;
+data.order.address = "ул. " + address.street + ", дом " + address.house + ", " + address.entrance + " подъезд, " + address.floor + " этаж, кв. " + address.flat;
+data.order.sum = sum;
+data.client = name + " " + phone;
+let jsonData = JSON.stringify({data});
+return jsonData;
 }  
+ 
+
  
